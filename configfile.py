1cb20c7e73b3320cf6566e27006391a44f07d405
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-01-16 05:18:20 +0100

    Add files via upload

---
+++ configfile.py
@@ -0,0 +1,9 @@
+tenant_id = '69123506-e623-429a-a461-74482242e5f2'
+client_id = 'eba80889-1602-4c06-b8f2-6b63445d88d2'
+client_secret = 'Kay8Q~Y4pIv5.y1a3cej6LdCFJFDxlgH3gHZRc7c'
+device_id = '6c037821-003c-4389-b59a-4499cefa22f8'
+log_file_path = 'C:/test/log.txt'
+image_file_path = 'C:/test/img.png'
+data_path = 'C:/test/userlist.csv'
+delay_time = 30
+use_service=True
