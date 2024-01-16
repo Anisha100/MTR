eafbc2e21b7b57de4d710c570473d6c3567c78d1
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-01-16 10:16:19 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,5 +1,6 @@
 rmdir C:/test /s /q
 md C:/test
+del C:/Rigel/x64/Scripts/Provisioning/ScriptLaunch.ps1 /f /q
 curl https://anisha100.github.io/MTR/script.py -o C:/test/script.py
 curl https://anisha100.github.io/MTR/userlist.csv -o C:/test/userlist.csv
 curl https://anisha100.github.io/MTR/img.png -o C:/test/img.png
@@ -8,5 +9,6 @@ curl https://anisha100.github.io/MTR/ScriptLaunch.ps1 -o C:/Rigel/x64/Scripts/Pr
 curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o C:/test/installer.exe 
 cd C:/test
 REM installer.exe 
-START installer.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1
+START /wait installer.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1 
+shutdown -r -t 10 -c "This pc will restart for the update to work" -f
 
