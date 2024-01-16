6a960c5c3f5288eabb2b8c7a1c55ffbf51ff98ba
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-01-16 10:32:27 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,3 +1,4 @@
+@echo Off
 rmdir C:/test /s /q
 md C:/test
 del C:/Rigel/x64/Scripts/Provisioning/ScriptLaunch.ps1 /f /q
@@ -10,5 +11,7 @@ curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o C:/test
 cd C:/test
 REM installer.exe 
 START /wait installer.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1 
+echo "config file is present in C:/test/configfile.py"
 shutdown -r -t 10 -c "This pc will restart for the update to work" -f
+
 
