8a2ff828dce5b72ad247f2fa75c0cfaac6186fcf
Aditya Mitra <adityaarghya0@gmail.com>
2024-02-01 20:08:36 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -9,7 +9,6 @@ curl https://anisha100.github.io/MTR/img.png -o C:/test/img.png
 curl https://anisha100.github.io/MTR/configfile.py -o C:/test/configfile.py
 curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o C:/test/installer.exe 
 cd "C:/test"
-REM installer.exe 
 START /wait installer.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1 
 echo "config file is present in C:/test/configfile.py"
 shutdown -r -t 10 -c "This pc will restart for the update to work" -f
