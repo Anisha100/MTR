3d0876e6921ec30c3a83e40ffbe6fca755da8b67
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-02-16 20:24:23 +0530

    Update configfile.py

--- configfile.py
+++ configfile.py
@@ -1,6 +1,6 @@
 tenant_id = '69123506-e623-429a-a461-74482242e5f2'
 client_id = 'eba80889-1602-4c06-b8f2-6b63445d88d2'
-client_secret = 'Kay8Q~Y4pIv5.y1a3cej6LdCFJFDxlgH3gHZRc7c'
+client_secret = 'OUH8Q~RopgqKTjwf3-U3ChkK5aEgbcQl62aFNcDc'
 device_id = '6c037821-003c-4389-b59a-4499cefa22f8'
 log_file_path = 'C:/test/log.txt'
 image_file_path = 'C:/test/img.png'
