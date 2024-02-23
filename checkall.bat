8b9101cb4c613daae0337ff175f41b1d2b91408e
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-02-23 12:12:19 +0530

    Update checkall.bat

--- checkall.bat
+++ checkall.bat
@@ -1,2 +1,2 @@
-python -c"import pandas as pd; from configfile import *; data= pd.read_csv('userlist.csv'); print( data['user id']); print(data['serial']);"
+python -c"import pandas as pd; from configfile import *; data= pd.read_csv(data_path); print( data['user id']); print(data['serial']);"
 pause
