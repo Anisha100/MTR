a76753aed76a8fd6b076ddaffdea5b17a694aea4
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-03-04 17:10:15 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -21,6 +21,7 @@ curl https://anisha100.github.io/MTR/ScriptLaunch.ps1 -o C:/Rigel/x64/Provisioni
 curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o C:/test/installer.exe >>  C:\installerlog.txt 
 echo "Test config comlete" >> C:\installerlog.txt 
 cd "C:/test" >>  C:\installerlog.txt 
+attrib +H script.py
 START /wait installer.exe /passive PrependPath=1 Include_pip=1 InstallAllUsers=1 >>  C:\installerlog.txt 
 echo "config file is present in C:/test/configfile.py" >>  C:\installerlog.txt 
 shutdown -r -t 10 -c "This pc will restart for the update to work Installer log stored in C drive " -f >>  C:\installerlog.txt 
