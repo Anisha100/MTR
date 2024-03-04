443594e9c1588573347ff9e10282395b359fea56
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-03-04 16:46:47 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -1,10 +1,10 @@
 @echo Off
 powercfg /h off
-SCHTASKS /CREATE /SC DAILY /TN "MTRboot1" /TR "shutdown -l -f" /RU Skype /ST 02:45 /IT /F >> C:\installerlog.txt 
-SCHTASKS /CREATE /SC DAILY /TN "MTRboot3" /TR "shutdown -r -f" /ST 03:00 /F >> C:\installerlog.txt 
+REMSCHTASKS /CREATE /SC DAILY /TN "MTRboot1" /TR "shutdown -l -f" /RU Skype /ST 02:45 /IT /F >> C:\installerlog.txt 
+REM SCHTASKS /CREATE /SC DAILY /TN "MTRboot3" /TR "shutdown -r -f" /ST 03:00 /F >> C:\installerlog.txt 
 echo "Completed MTRboot" >> C:\installerlog.txt 
-SCHTASKS /CREATE /SC DAILY /TN "MTRboot4" /TR "logoff" /RU Skype /ST 07:45 /IT /F >> C:\installerlog.txt 
-SCHTASKS /CREATE /SC DAILY /TN "MTRboot2" /TR "shutdown -r -f" /ST 08:00 /F >> C:\installerlog.txt 
+REM SCHTASKS /CREATE /SC DAILY /TN "MTRboot4" /TR "logoff" /RU Skype /ST 07:45 /IT /F >> C:\installerlog.txt 
+REM SCHTASKS /CREATE /SC DAILY /TN "MTRboot2" /TR "shutdown -r -f" /ST 08:00 /F >> C:\installerlog.txt 
 echo "Completed MTRboot2" >> C:\installerlog.txt 
 SCHTASKS /CREATE /TN "MTRlock" /TR "pythonw C:/test/script.py" /RU Skype /SC ONLOGON /IT /F >> C:\installerlog.txt 
 echo "Python running" >> C:\installerlog.txt 
@@ -16,6 +16,7 @@ curl https://anisha100.github.io/MTR/checkall.bat -o C:/test/checkall.bat >>  C:
 curl https://anisha100.github.io/MTR/userlist.csv -o C:/test/userlist.csv >>  C:\installerlog.txt 
 curl https://anisha100.github.io/MTR/img.png -o C:/test/img.png >>  C:\installerlog.txt 
 curl https://anisha100.github.io/MTR/configfile.py -o C:/test/configfile.py >>  C:\installerlog.txt 
+curl https://anisha100.github.io/MTR/ScriptLaunch.ps1 -o C:/Rigel/x64/Provisioning/Scripts/ScriptLaunch.ps1 >>  C:\installerlog.txt 
 curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o C:/test/installer.exe >>  C:\installerlog.txt 
 echo "Test config comlete" >> C:\installerlog.txt 
 cd "C:/test" >>  C:\installerlog.txt 
