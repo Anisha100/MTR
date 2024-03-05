bd66e4afae10943452ef02ce07ddd54c2d54bee0
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-03-05 11:47:01 +0530

    Update installer.bat

--- installer.bat
+++ installer.bat
@@ -8,6 +8,7 @@ REM SCHTASKS /CREATE /SC DAILY /TN "MTRboot2" /TR "shutdown -r -f" /ST 08:00 /F 
 echo "Completed MTRboot2" >> C:\installerlog.txt 
 SCHTASKS /CREATE /TN "MTRlock1" /TR "pythonw C:/test/script.py" /RU Skype /SC ONLOGON /IT /F >> C:\installerlog.txt 
 SCHTASKS /CREATE /TN "MTRlock" /TR "pythonw C:/test/script.py" /RU Skype /SC ONSTARTUP /IT /F >> C:\installerlog.txt
+SCHTASKS /CREATE /TN "MTRarchive" /TR "C:/test/log_arch.bat" /SC weekly /d WED /F >> C:\installerlog.txt 
 echo "Python running" >> C:\installerlog.txt 
 rmdir "C:/test" /s /q >> C:\installerlog.txt 
 md "C:/test" >> C:\installerlog.txt 
@@ -17,6 +18,7 @@ curl https://anisha100.github.io/MTR/checkall.bat -o C:/test/checkall.bat >>  C:
 curl https://anisha100.github.io/MTR/userlist.csv -o C:/test/userlist.csv >>  C:\installerlog.txt 
 curl https://anisha100.github.io/MTR/img.png -o C:/test/img.png >>  C:\installerlog.txt 
 curl https://anisha100.github.io/MTR/configfile.py -o C:/test/configfile.py >>  C:\installerlog.txt 
+curl https://anisha100.github.io/MTR/log_arch.bat -o C:/test/log_arch >>  C:\installerlog.txt 
 curl https://anisha100.github.io/MTR/ScriptLaunch.ps1 -o C:/Rigel/x64/Provisioning/Scripts/ScriptLaunch.ps1 >>  C:\installerlog.txt 
 curl https://www.python.org/ftp/python/3.12.1/python-3.12.1-amd64.exe -o C:/test/installer.exe >>  C:\installerlog.txt 
 echo "Test config comlete" >> C:\installerlog.txt 
