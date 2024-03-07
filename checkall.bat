f73c41ac7bcf29a7749e5fcb24df25c834a29b94
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-03-07 11:13:22 +0530

    Update checkall.bat

--- checkall.bat
+++ checkall.bat
@@ -1,2 +1,2 @@
-python -c "import pandas as pd; from configfile import *; os.system('SCHTASKS /CREATE /TN MTRarchive /TR C:/test/log_arch.bat /SC daily /mo'+ str(log_date) +'/F') ; data= pd.read_csv(data_path); print( data['user id']); print(data['serial']);"
+python -c "import pandas as pd; from configfile import *; os.system('SCHTASKS /CREATE /TN MTRarchive /TR C:/test/log_arch.bat /SC daily /mo '+ str(log_date) +' /F') ; data= pd.read_csv(data_path); print( data['user id']); print(data['serial']);"
 pause
