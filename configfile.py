0463041f5d214a74bccd9a898d227f237d254734
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-03-07 09:11:15 +0530

    Update configfile.py

--- configfile.py
+++ configfile.py
@@ -1,11 +1,10 @@
 tenant_id = '69123506-e623-429a-a461-74482242e5f2'
 client_id = 'eba80889-1602-4c06-b8f2-6b63445d88d2'
 client_secret = 'OUH8Q~RopgqKTjwf3-U3ChkK5aEgbcQl62aFNcDc'
-device_id = '6c037821-003c-4389-b59a-4499cefa22f8'
 log_file_path = 'C:/test/log.txt'
 image_file_path = 'C:/test/img.png'
 data_path = 'C:/test/userlist.csv'
 delay_time = 30
 use_service=True
 log_date=7
-os.system("SCHTASKS /CREATE /TN "MTRarchive" /TR "C:/test/log_arch.bat" /SC daily /mo "+str(log_date)+" /F ")
+os.system("SCHTASKS /CREATE /TN MTRarchive /TR C:/test/log_arch.bat /SC daily /mo "+str(log_date)+" /F ")
