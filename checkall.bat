a6e2f757bd7bd5cb2e0945af200fa8bcbe567de7
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-03-07 11:06:37 +0530

    Update checkall.bat

--- checkall.bat
+++ checkall.bat
@@ -1,4 +1,2 @@
-from configfile.py import *
-SCHTASKS /CREATE /TN "MTRarchive" /TR "C:/test/log_arch.bat" /SC daily /mo str(log_date) /F 
 python -c "import pandas as pd; from configfile import *; data= pd.read_csv(data_path); print( data['user id']); print(data['serial']);"
 pause
