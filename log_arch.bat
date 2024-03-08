f01c6c04f2cd41aca548764e0ee3634bbbdc9f4d
Anisha Ghosh <87559560+Anisha100@users.noreply.github.com>
2024-03-08 10:39:37 +0530

    Update log_arch.bat

--- log_arch.bat
+++ log_arch.bat
@@ -1,7 +1,7 @@
 @echo off
 For /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%a-%%b)
-mkdir C:/test/Archive_folder
-mv C:/test/log.txt C:/test/Archive_folder/log.txt
+md C:/test/Archive_folder
+move C:/test/log.txt C:/test/Archive_folder/log.txt
 ren C:test/Archive_folder/log.txt log-%mydate%.txt
 
 
